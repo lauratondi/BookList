d2fef3986791ce2cf3f7f7a791a932babfb7c37b
Lauramars <lauramars85@gmail.com>
2020-09-04 11:25:00 +0200

    Book List UI & Add Book to List

---
+++ app.js
@@ -0,0 +1,53 @@
+// Book Constructor
+function Book(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
+
+// UI Constructor
+function UI() {}
+
+// Add book to list
+UI.prototype.addBookToList = function (book) {
+  const list = document.getElementById('book-list');
+  // create tr element
+  const row = document.createElement('tr');
+  // Insert cols
+  row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete"> X <a></td>
+    `;
+
+  list.appendChild(row);
+};
+
+// Clear Fields
+UI.prototype.clearFields = function () {
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
+  document.getElementById('isbn').value = '';
+};
+
+// Event listeners
+document.getElementById('book-form').addEventListener('submit', function (e) {
+  // Get form values
+  const title = document.getElementById('title').value,
+    author = document.getElementById('author').value,
+    isbn = document.getElementById('isbn').value;
+
+  // Instatiate book
+  const book = new Book(title, author, isbn);
+
+  // Instatiate UI
+  const ui = new UI();
+
+  //   Add book to list
+  ui.addBookToList(book);
+
+  // Clear Fields
+  ui.clearFields();
+  e.preventDefault();
+});
